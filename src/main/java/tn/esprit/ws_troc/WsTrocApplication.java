8b74a1e545cf6c72b589e81a3880a8f1ac802267
houssembalti <houssem.balti@esprit.tn>
2023-10-30 20:21:05 +0100

    Initial commit

---
+++ src/main/java/tn/esprit/ws_troc/WsTrocApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.ws_troc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsTrocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsTrocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/ws_troc/WsTrocApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.ws_troc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsTrocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
